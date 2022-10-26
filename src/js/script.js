472dd9f8e91b661f6c81fe596266c2cf37d85088
Эргард Бехерт <is53-behertev@radiotech.su>
2022-10-26 17:14:00 +0500

    first

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header-body__menu');
+
+burger.onclick = function() {
+    burger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    document.body.classList.toggle('lock');
+}
